b3fc68d254b15e8aa226f6ed16cf09a17fba8bfa
Simone-DF <s.dellefratte@gruppointent.com>
2025-10-29 16:52:28 +0100

    Populate LetMeNowBackEndPython: Whisper transcription API

--- function/whisper.py
+++ function/whisper.py
@@ -7,16 +7,16 @@ device = "cpu"
 valid_models = ["tiny", "base", "large", "turbo"]
 
 # Specifica il percorso della cartella in cui scaricare i modelli
-DOWNLOAD_FOLDER = "C:/Users/SimoneDelleFratte/Desktop/whisper_api/models"  # Sostituisci con il percorso desiderato
+DOWNLOAD_FOLDER = "C:/Users/SimoneDelleFratte/Desktop/LetMeNow/whisper_api/models"  # Sostituisci con il percorso desiderato
 
 def load_whisper_model(model_name: str):
     """
     Carica il modello Whisper specificato.
-    Se il modello non è valido, utilizza 'large' come default.
+    Se il modello non è valido, utilizza 'turbo' come default.
     """
     if model_name not in valid_models:
-        print("Modello non valido. Verrà utilizzato il modello 'large' di default.")
-        model_name = "large"
+        print("Modello non valido. Verrà utilizzato il modello 'turbo' di default.")
+        model_name = "turbo"
     model = whisper.load_model(model_name, device=device, download_root=DOWNLOAD_FOLDER)
     return model
 
